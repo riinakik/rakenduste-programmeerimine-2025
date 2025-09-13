4faf806a85316de948a73f026a5fe68404fb3549
Riina Kikkas <riinakik@tlu.ee>
2025-09-13 15:54:38 +0300

    initial commit

---
+++ 2025-09-11/my-app/src/components/Counter.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+import "../App.css"
+
+function Counter() {
+  const [count, setCount] = useState(0)
+
+  function increaseCounter(amount: number) {
+    setCount(count => count + amount)
+  }
+
+  return (
+    <>
+      <h1>Vite + React + Riina</h1>
+      <div className="card">
+        <button onClick={() => increaseCounter(10)}>count is {count}</button>
+      </div>
+    </>
+  )
+}
+
+export default Counter
