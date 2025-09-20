14f3d53b1d1442ea147b2d9b9860c9831f89869d
Riina Kikkas <riinakik@tlu.ee>
2025-09-20 14:44:52 +0300

    fix(build): remove unused React import from main.tsx

--- 2025-09-18/my-app/src/main.tsx
+++ 2025-09-18/my-app/src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { CssBaseline, StyledEngineProvider } from "@mui/material";
